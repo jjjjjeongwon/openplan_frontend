f56006b4b7942be3179e7caaf6a84e5418a35187
jjjjjeongwon <jjjjjwon21@gmail.com>
2026-02-15 15:27:54 +0900

    feat: 404 페이지 추가

---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import styles from './not-found.module.css';
+import { Button } from '@repo/ui/button';
+
+export default function NotFoundPage() {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.description}>페이지를 찾을 수 없습니다.</p>
+      <Button onClick={() => router.push('/')} children="메인으로 돌아가기" />
+    </div>
+  );
+}
